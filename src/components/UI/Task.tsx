7ca486522c1ad8d3d3504314c41e6c3ef7eccca9
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-12-11 12:39:52 +0530

    basic task template

--- src/components/UI/Task.tsx
+++ src/components/UI/Task.tsx
@@ -1,9 +1,17 @@
-const Task = () => {
-  return (
-    <div className="">
-        asdasdasdasd
-    </div>
-  )
+import "./Task.css"
+import ITask from "../../assets/ITask"
+
+const Task = (props: any) => {
+	let noteData = props.noteMetaData;
+
+	return (
+		<div className="note">
+			<span>{noteData.title??"null"}</span>
+			<span>{noteData.note??"null"}</span>
+			<span>{noteData.status??"null"}</span>
+			<span>{noteData.priority??"null"}</span>
+		</div>
+	)
 }
 
 export default Task
