216e89bfca03c58756c8acd5f03508d3be499221
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-12-11 12:38:10 +0530

    data is now available in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import "./App.css";
+import { useState } from "react";
+import ITask from "./assets/ITask";
 
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 
 function App() {
+	const [tasks, setTasks] : [any, any]= useState([]);
+
+	const addData = (data : ITask) => {
+		let newTaskArray = [...tasks, data];
+		setTasks(newTaskArray);
+	}
+
 	return (
 		<>
-			<Header></Header>
-			<Main></Main>
+			<Header addNote={addData}></Header>
+			<Main tasks={tasks}></Main>
 			<Footer></Footer>
 		</>
 	);
