cf59860081971f8ef89d06e9331d21c2e41d5aee
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-12-12 11:58:28 +0530

    editDialog now closes and has two way binding

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,18 @@ import ITask from "./assets/ITask";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
+import EditTaskDropdown from "./components/UI/EditTaskDropdown";
 
 function App() {
 	// Initialise data from local storage
 	const localData = localStorage.getItem("GET_003_Chapter_010");
 	const sessionData = localData ? JSON.parse(localData) : [];
-	const [darkMode, setDarkMode] = useState(false);
-	
-	let [tasks, setTasks]: [any, any] = useState(sessionData);
 
+	let [darkMode, setDarkMode] = useState(false);
+	let [tasks, setTasks]: [any, any] = useState(sessionData);
+	let [editTaskDialogVisible, setEditTaskDialogVisible] = useState(false);
+
+	// Add, Edit and Delete Functions
 	let addData = (newNote : ITask) => {
 		// Dont add note if both fields empty
 		if(newNote.title === "" && newNote.note === "") return;
@@ -25,6 +28,16 @@ function App() {
 		localStorage.setItem("GET_003_Chapter_010", JSON.stringify(newTaskArray));
 	}
 
+	let [taskToEdit, setTaskToEdit] = useState({});
+	let editNote = (key: string) => {
+		let selectedTask = tasks.filter((task: ITask) => {
+			return task.key === key;
+		})[0];
+
+		setTaskToEdit(selectedTask);
+		setEditTaskDialogVisible(true);
+	}
+
 	let deleteNote = (key: string) => {
 		let updatedTaskArray = tasks.filter((task: ITask) => {
 			return task.key != key;
@@ -34,6 +47,25 @@ function App() {
 		// Save to local storage
 		localStorage.setItem("GET_003_Chapter_010", JSON.stringify(updatedTaskArray));
 	}
+
+	// Edit Dialog Functions
+	let saveTaskToList = (
+		key: string, 
+		title: string, 
+		note: string, 
+		status: string, 
+		priority: string, 
+		date: string, 
+		time: string
+	) => {
+		let selectedTask = tasks.filter((task: ITask) => {
+			if(task.key == key){
+				task.time = title;
+			}
+		})[0];
+		setEditTaskDialogVisible(false);
+	}
+	let cancelEdit = () => setEditTaskDialogVisible(false);
 
 	// Toggle Mode
 	let changeTheme = () => {
@@ -63,8 +95,11 @@ function App() {
 
 	return (
 		<>
+			{
+				editTaskDialogVisible ? <EditTaskDropdown data={taskToEdit} saveTask={saveTaskToList} cancelEdit={cancelEdit}></EditTaskDropdown> : ""
+			}
 			<Header addNote={addData} toggleMode={toggleMode}></Header>
-			<Main tasks={tasks} deleteNote={deleteNote}></Main>
+			<Main tasks={tasks} editNote={editNote} deleteNote={deleteNote}></Main>
 			<Footer></Footer>
 		</>
 	);
--- src/components/UI/EditTaskDropdown.tsx
+++ src/components/UI/EditTaskDropdown.tsx
@@ -1,5 +1,5 @@
 import "./NewTaskDropdown.css"
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 const EditTaskDropdown = (props: any) => {
     let taskTitle = useRef(null);
@@ -9,6 +9,13 @@ const EditTaskDropdown = (props: any) => {
     let taskDate = useRef(null);
     let taskTime = useRef(null);
 
+    let [title, setTitle] = useState(props.data.title);
+    let [note, setNote] = useState(props.data.note);
+    let [status, setStatus] = useState(props.data.status);
+    let [priority, setPriority] = useState(props.data.priority);
+    let [date, setDate] = useState(props.data.date);
+    let [time, setTime] = useState(props.data.time);
+
     let save = () => {
         let title = taskTitle.current ? (taskTitle.current as HTMLInputElement).value : "";
         let note = taskNote.current ? (taskNote.current as HTMLInputElement).value : "";
@@ -17,23 +24,47 @@ const EditTaskDropdown = (props: any) => {
         let date = taskDate.current ? (taskDate.current as HTMLInputElement).value : "";
         let time = taskTime.current ? (taskTime.current as HTMLInputElement).value : "";
 
-        props.saveTask(title, note, status, priority, date, time);
+        props.saveTask(props.data.key, title, note, status, priority, date, time);
     }
 
     return (
         <div id="new_task_dialog_bg">
             <form id="new_task_dialog">
-                <input type="text" name="title" ref={taskTitle} id="new_task_title" className="flat new_task_dialog_element" placeholder="Note Title"/>
+                <input 
+                    type="text" 
+                    name="title" 
+                    ref={taskTitle} 
+                    id="new_task_title" 
+                    className="flat new_task_dialog_element" 
+                    placeholder="Note Title" 
+                    value={title}
+                    onChange={(e)=>setTitle(e.target.value)}
+                />
 
                 <div id="new_task_dialog_metadata" className="new_task_dialog_element">
-                    <input type="date" name="new_task_date" ref={taskDate} id="" className="flat"/>
-                    <input type="time" name="new_task_time" ref={taskTime} id="" />
-                    <select name="" ref={taskStatus} id="" className="flat">
+                    <input 
+                        type="date" 
+                        name="new_task_date" 
+                        ref={taskDate} 
+                        id="" 
+                        className="flat" 
+                        value={Date.parse(date)}
+                        onChange={(e)=>setDate(e.target.value)}
+                    />
+                    <input 
+                        type="time" 
+                        name="new_task_time" 
+                        ref={taskTime} 
+                        id="" 
+                        value={time}
+                        onChange={(e)=>setTime(e.target.value)}
+                    />
+                    <select name="" ref={taskStatus} id="" className="flat" value={status} onChange={(e)=>setStatus(e.target.value)}>
                         <option value="new">New</option>
                         <option value="inprogress">In Progress</option>
                         <option value="complete">Completed</option>
                     </select>
-                    <select name="" ref={taskPriority} id="" className="flat">
+                    <select name="" ref={taskPriority} id="" className="flat" value={priority} onChange={(e)=>setPriority(e.target.value)}>
                         <option value="highest">Highest</option>
                         <option value="high">High</option>
                         <option value="normal">Normal</option>
@@ -43,7 +74,15 @@ const EditTaskDropdown = (props: any) => {
                     </select>
                 </div>
 
-                <textarea name="" ref={taskNote} id="new_task_note" className="flat new_task_dialog_element" placeholder="Write a note..."></textarea>
+                <textarea 
+                    name="" 
+                    ref={taskNote} 
+                    id="new_task_note" 
+                    className="flat new_task_dialog_element" 
+                    placeholder="Write a note..." 
+                    value={note}
+                    onChange={(e)=>setNote(e.target.value)}
+                ></textarea>
 
                 <div id="new_task_dialog_button_wrapper" className="new_task_dialog_element">
                     <button type="button" id="new_task_cancel_button" className="flat_button" onClick={props.cancelEdit}>Cancel</button>
